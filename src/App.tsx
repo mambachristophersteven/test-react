04a4404e0632d9c2faf2c0eab35d477e84c520f1
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-22 14:59:44 +0000

    navbar component setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <div className="App">
-    
+    <Navbar/>
     </div>
   )
 }
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+function Navbar() {
+
+    return (
+      <div className="navbar">
+        <h1>hello</h1>
+      </div>
+    )
+  }
+  
+  export default Navbar
