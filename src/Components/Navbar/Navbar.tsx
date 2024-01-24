903993768f63292f58e95eda9edc1f68e4629e1e
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-24 18:50:23 +0000

    navbar content added

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,7 +4,22 @@ function Navbar() {
 
     return (
       <div className="navbar">
-        <h1>hello</h1>
+        <div className="logo">
+          <img src="./black-hat.svg" alt="logo" />
+        </div>
+        <ul className="nav-menu">
+          <li>
+            <a href="#">home</a>
+            <img src="./ellipse.svg" alt="" />
+          </li>
+          <li><a href="#">categories</a></li>
+          <li><a href="#">leaderboards</a></li>
+          <li><a href="#">what's new?</a></li>
+        </ul>
+        <div className="accounts">
+          <a href="#">sign in</a>
+          <button>create account</button>
+        </div>
       </div>
     )
   }
